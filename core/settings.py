8ddf0c7da27fa44f0469edc732229dc37bdb3a43
Taha <TahaM8000@gmail.com>
2022-08-16 23:29:20 +0430

    redesign Set info

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'panel',
     'accounts',
     'info',
     'map',
--- info/views.py
+++ info/views.py
@@ -12,17 +12,17 @@ def setReason(request,username,reason):
     user.save()
 
 
-    return render(request,'info/setReasonPage.html')
+    return render(request,'info/sets/setReasonPage.html')
 #---------------------------------------------------------------------------------------
 def setReasonlist(request,reason):
     users = User.objects.filter(reason = reason)
     amount = 0
     for user in users:
         amount += 1
-    return render(request,'info/setReason.html',{'users' : users,'amount':amount,'reason':reason})
+    return render(request,'info/sets/setReason.html',{'users' : users,'amount':amount,'reason':reason})
 #---------------------------------------------------------------------------------------
 def setReasonPage(request):
-    return render(request,'info/setReasonPage.html')
+    return render(request,'info/sets/setReasonPage.html')
 #---------------------------------------------------------------------------------------
 def charts(request):
     return render(request,'info/charts/chartList.html')
